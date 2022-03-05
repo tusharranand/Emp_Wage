1cc2be7f69a81e9872a8372bd857ae457fdfd4ca
tusharranand <tusharranand@gmail.com>
2022-03-05 14:54:01 +0530

    UC1 successfully achieved

---
+++ EmpWage/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+Console.WriteLine("Welcome to Employee Wage Computation Program.");
+
+// UC 1
+
+int is_present = 1;
+
+Random check = new Random();
+int empCheck = check.Next(0, 2);
+
+if (empCheck == is_present)
+{
+    Console.WriteLine("Employee is present.");
+}
+else
+{
+    Console.WriteLine("Employee is absent.");
+}
