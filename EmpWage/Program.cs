13d8acef9cd1021d08bcc571ef23f16fbc31d61a
tusharranand <tusharranand@gmail.com>
2022-03-05 16:01:40 +0530

    UC3 successfully achieved

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -2,24 +2,43 @@
 
 Console.WriteLine("Welcome to Employee Wage Computation Program.");
 
-// UC 2
+// UC 3
 
 int is_present = 1;
 int wage_per_hr = 20;
 int hr_per_day = 0;
 int wage_per_day = 0;
+int full_time = 1;
 
 Random check = new Random();
-int empCheck = check.Next(0, 2);
+int emp_time = check.Next(0, 2);
 
-if (empCheck == is_present)
+Random check2 = new Random();
+int empCheck = check2.Next(0, 2);
+
+if (emp_time == full_time)
 {
-    Console.WriteLine("Employee is present.");
     hr_per_day = 8;
+    if (empCheck == is_present)
+    {
+        Console.WriteLine("Full time employee is present.");
+    }
+    else
+    {
+        Console.WriteLine("Full time employee is absent.");
+    }
 }
 else
 {
-    Console.WriteLine("Employee is absent.");
+    hr_per_day = 4;
+    if (empCheck == is_present)
+    {
+        Console.WriteLine("Part time employee is present.");
+    }
+    else
+    {
+        Console.WriteLine("Part time employee is absent.");
+    }
 }
 
 wage_per_day = (wage_per_hr * hr_per_day);
