37e1249c88d7e99825cadf50aed54361ee967ec7
tusharranand <tusharranand@gmail.com>
2022-03-05 15:52:48 +0530

    UC2 successfully achieved

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -2,9 +2,12 @@
 
 Console.WriteLine("Welcome to Employee Wage Computation Program.");
 
-// UC 1
+// UC 2
 
 int is_present = 1;
+int wage_per_hr = 20;
+int hr_per_day = 0;
+int wage_per_day = 0;
 
 Random check = new Random();
 int empCheck = check.Next(0, 2);
@@ -12,8 +15,12 @@ int empCheck = check.Next(0, 2);
 if (empCheck == is_present)
 {
     Console.WriteLine("Employee is present.");
+    hr_per_day = 8;
 }
 else
 {
     Console.WriteLine("Employee is absent.");
 }
+
+wage_per_day = (wage_per_hr * hr_per_day);
+Console.WriteLine("Daily wage = " + wage_per_day);
