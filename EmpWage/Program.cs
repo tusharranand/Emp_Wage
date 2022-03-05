a624b812ad586f7db373cd6dd1f096171dcbe046
tusharranand <tusharranand@gmail.com>
2022-03-05 16:38:27 +0530

    UC5 for 20 days

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -2,33 +2,43 @@
 
 Console.WriteLine("Welcome to Employee Wage Computation Program.");
 
-// UC 4
+// UC 5
 
+int is_present = 1;
+int full_time = 1;
 int wage_per_hr = 20;
 int hr_per_day = 0;
 int wage_per_day = 0;
-
-// absent = 0;
-// full_time = 1;
-// part_time = 2;
+int present_days = 0;
+int wage_per_month = 0;
 
 Random check = new Random();
-int status = check.Next(0, 3);
+int emp_time = check.Next(0, 2);
 
-switch (status)
+if (emp_time == full_time)
 {
-    case 0:
-        Console.WriteLine("Employee is absent.");
-        break;
-    case 1:
-        Console.WriteLine("Full time employee is present.");
-        hr_per_day = 8;
-        break;
-    case 2:
-        Console.WriteLine("Part time employee is present.");
-        hr_per_day = 4;
-        break;
+    hr_per_day = 8;
+}
+else
+{
+    hr_per_day = 4;
+}
+
+for (int i = 0; i < 20; i++) { 
+
+    Random check2 = new Random();
+    int empCheck = check2.Next(0, 2);
+
+    if (empCheck == is_present)
+    {
+        present_days++;
+    }
 }
 
 wage_per_day = (wage_per_hr * hr_per_day);
 Console.WriteLine("Daily wage = " + wage_per_day);
+
+Console.WriteLine("Days present = " + present_days);
+
+wage_per_month = (wage_per_day * present_days);
+Console.WriteLine("Monthly wage = " + wage_per_month);
