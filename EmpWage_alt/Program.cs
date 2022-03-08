704ae81b7d05e93c6cefef159d33633cb1c10ece
Tushar Anand <tusharranand@gmail.com>
2022-03-08 14:25:05 +0530

    Same as UC6 but using classes, objects and methods

--- EmpWage_alt/Program.cs
+++ EmpWage_alt/Program.cs
@@ -1,54 +1,70 @@
 ﻿using System;
-
-// UC 6
-
-namespace EmpWage
+// UC7
+namespace EmpWage_alt
 {
-    class Program
+    class Wage_Computation
     {
-        public const int full_time = 1;
-        public const int part_time = 2;
-        public const int wage_per_hr = 20;
-        public const int working_days = 20;
-        public const int max_working_hrs = 100;
+        public const int Full_Time = 1;
+        public const int Part_Time = 2;
+        public int Wage_Per_Hr = 20;
+        public int Hr_Per_Day = 0;
+        public int Full_Hr_Per_Day = 0;
+        public int Part_Hr_Per_Day = 0;
+        public int Wage_Per_Day = 0;
+        public int Total_Working_Hrs = 0;
+        public int Max_Working_Days = 20;
+        public int Max_Working_Hrs = 100;
+        public int Present_Days = 0;
+        public int Monthly_Wage = 0;
 
-        public static void Main(string[] args)
+        public Wage_Computation(int Wage_Per_Hr, int Full_Hr_Per_Day, int Part_Hr_Per_Day, int Max_Working_Hrs, int Max_Working_Days)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program.");
+            this.Wage_Per_Hr = Wage_Per_Hr;
+            this.Full_Hr_Per_Day = Full_Hr_Per_Day;
+            this.Part_Hr_Per_Day = Part_Hr_Per_Day;
+            this.Max_Working_Hrs = Max_Working_Hrs;
+            this.Max_Working_Days = Max_Working_Days;
+        }
 
-            int full_day_hr = 0;
-            int wage_per_day = 0;
-            int monthly_wage = 0;
-            int total_working_hrs = 0;
-            int present_days = 0;
+        private int Present_Check()
+        {
+            return new Random().Next(0, 3);
+        }
 
-            while (total_working_hrs <= max_working_hrs && present_days <= working_days)
+        public void Calculations()
+        {
+            while (Total_Working_Hrs <= Max_Working_Hrs && Present_Days <= Max_Working_Days)
             {
-                Random check = new Random();
-                int status = check.Next(0, 3);
 
-                switch (status)
+                switch (Present_Check())
                 {
-                    case full_time:
-                        full_day_hr = 8;
-                        present_days++;
+                    case Full_Time:
+                        Hr_Per_Day = Full_Hr_Per_Day;
+                        Present_Days++;
                         break;
-                    case part_time:
-                        full_day_hr = 4;
-                        present_days++;
+                    case Part_Time:
+                        Hr_Per_Day = Part_Hr_Per_Day;
+                        Present_Days++;
                         break;
                     default:
-                        full_day_hr = 0;
+                        Hr_Per_Day= 0;
                         break;
                 }
-                total_working_hrs += full_day_hr;
-                wage_per_day = (wage_per_hr * full_day_hr);
-                monthly_wage += wage_per_day;
-                Console.WriteLine("Daily wage = " + wage_per_day);
+                Total_Working_Hrs += Hr_Per_Day;
+                Wage_Per_Day = (Wage_Per_Hr * Hr_Per_Day);
+                Monthly_Wage += Wage_Per_Day;
+                Console.WriteLine("Daily wage = " + Wage_Per_Day);
             }
-            Console.WriteLine("Total hours worked = " + total_working_hrs);
-            Console.WriteLine("Total days present = " + present_days);
-            Console.WriteLine("Monthly wage = " + monthly_wage);
+            Console.WriteLine("Total hours worked = " + Total_Working_Hrs);
+            Console.WriteLine("Total days present = " + Present_Days);
+            Console.WriteLine("Monthly wage = " + Monthly_Wage);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program.");
+            Wage_Computation First_Employee = new Wage_Computation(20, 8, 4, 100, 20);
+            First_Employee.Calculations();
         }
     }
 }
