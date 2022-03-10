074298cf25568b56ea6732f198f6715a81766f08
tusharranand <99305968+tusharranand@users.noreply.github.com>
2022-03-10 15:26:32 +0530

    Update Program.cs

--- EmpWage_alt/Program.cs
+++ EmpWage_alt/Program.cs
@@ -2,37 +2,37 @@
 {
     class Program
     {
-        public const int full_time = 1;
-        public const int part_time = 2;
-        public const int wage_per_hr = 20;
+        public const int Full_Time = 1;
+        public const int Part_Time = 2;
+        public const int Wage_Per_Hr = 20;
 
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program.");
 
-            int full_day_hr = 0;
-            int wage_per_day = 0;
+            int Full_Day_Hr = 0;
+            int Wage_Per_Day = 0;
 
             Random check = new Random();
-            int status = check.Next(0, 3);
+            int Status = check.Next(0, 3);
 
-            switch (status)
+            switch (Status)
             {
-                case full_time:
+                case Full_Time:
                     Console.WriteLine("Full time employee is present.");
-                    full_day_hr = 8;
+                    Full_Day_Hr = 8;
                     break;
-                case part_time:
+                case Part_Time:
                     Console.WriteLine("Part time employee is present.");
-                    full_day_hr = 4;
+                    Full_Day_Hr = 4;
                     break;
                 default:
                     Console.WriteLine("Employee is absent.");
                     break;
             }
 
-            wage_per_day = (wage_per_hr * full_day_hr);
-            Console.WriteLine("Daily wage = " + wage_per_day);
+            Wage_Per_Day = (Wage_Per_Hr * Full_Day_Hr);
+            Console.WriteLine("Daily wage = " + Wage_Per_Day);
         }
     }
 }
