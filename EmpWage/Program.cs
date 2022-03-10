4922589dbef57a31b40419c8697bb5b2ca947c38
tusharranand <99305968+tusharranand@users.noreply.github.com>
2022-03-10 15:36:44 +0530

    Update Program.cs

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -4,42 +4,36 @@ Console.WriteLine("Welcome to Employee Wage Computation Program.");
 
 // UC 3
 
-int is_present = 1;
-int wage_per_hr = 20;
-int hr_per_day = 0;
-int wage_per_day = 0;
-int full_time = 1;
+int Is_Present = 1;
+int Wage_Per_Hr = 20;
+int Hr_Per_Day = 0;
+int Wage_Per_Day = 0;
+int Full_Time = 1;
 
 Random check = new Random();
-int emp_time = check.Next(0, 2);
+int Emp_Time = check.Next(0, 2);
 
-Random check2 = new Random();
-int empCheck = check2.Next(0, 2);
+Random Present = new Random();
+int EmpCheck = Present.Next(0, 2);
 
-if (emp_time == full_time)
+if (EmpCheck == Is_Present)
 {
-    hr_per_day = 8;
-    if (empCheck == is_present)
+    
+    if (Emp_Time == Full_Time)
     {
+        Hr_Per_Day = 8;
         Console.WriteLine("Full time employee is present.");
     }
     else
     {
-        Console.WriteLine("Full time employee is absent.");
+        Hr_Per_Day = 4;
+        Console.WriteLine("Part time employee is present.");
     }
 }
 else
 {
-    hr_per_day = 4;
-    if (empCheck == is_present)
-    {
-        Console.WriteLine("Part time employee is present.");
-    }
-    else
-    {
-        Console.WriteLine("Part time employee is absent.");
-    }
+        Console.WriteLine("Employee is absent.");
 }
 
-wage_per_day = (wage_per_hr * hr_per_day);
-Console.WriteLine("Daily wage = " + wage_per_day);
+Wage_Per_Day = (Wage_Per_Hr * Hr_Per_Day);
+Console.WriteLine("Daily wage = " + Wage_Per_Day);
