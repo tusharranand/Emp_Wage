9fdc050b78d74f4519a61b9cb34d741fa5707958
tusharranand <99305968+tusharranand@users.noreply.github.com>
2022-03-10 15:23:05 +0530

    Update Program.cs

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -4,31 +4,31 @@ Console.WriteLine("Welcome to Employee Wage Computation Program.");
 
 // UC 4
 
-int wage_per_hr = 20;
-int hr_per_day = 0;
-int wage_per_day = 0;
+int Wage_Per_Hr = 20;
+int Hr_Per_Day = 0;
+int Wage_Per_Day = 0;
 
 // absent = 0;
 // full_time = 1;
 // part_time = 2;
 
 Random check = new Random();
-int status = check.Next(0, 3);
+int Status = check.Next(0, 3);
 
-switch (status)
+switch (Status)
 {
     case 0:
         Console.WriteLine("Employee is absent.");
         break;
     case 1:
         Console.WriteLine("Full time employee is present.");
-        hr_per_day = 8;
+        Hr_Per_Day = 8;
         break;
     case 2:
         Console.WriteLine("Part time employee is present.");
-        hr_per_day = 4;
+        Hr_Per_Day = 4;
         break;
 }
 
-wage_per_day = (wage_per_hr * hr_per_day);
-Console.WriteLine("Daily wage = " + wage_per_day);
+Wage_Per_Day = (Wage_Per_Hr * Hr_Per_Day);
+Console.WriteLine("Daily wage = " + Wage_Per_Day);
