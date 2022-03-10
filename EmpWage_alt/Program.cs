a7e34d754bd7c845ea9da82d7a0788cc61212f59
tusharranand <99305968+tusharranand@users.noreply.github.com>
2022-03-10 12:30:05 +0530

    Update Program.cs

--- EmpWage_alt/Program.cs
+++ EmpWage_alt/Program.cs
@@ -6,49 +6,49 @@ namespace EmpWage
 {
     class Program
     {
-        public const int full_time = 1;
-        public const int part_time = 2;
-        public const int wage_per_hr = 20;
-        public const int working_days = 20;
-        public const int max_working_hrs = 100;
+        public const int Full_Time = 1;
+        public const int Part_Time = 2;
+        public const int Wage_Per_Hr = 20;
+        public const int Working_Days = 20;
+        public const int Max_Working_Hrs = 100;
 
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program.");
 
-            int full_day_hr = 0;
-            int wage_per_day = 0;
-            int monthly_wage = 0;
-            int total_working_hrs = 0;
-            int present_days = 0;
+            int Full_Day_Hr = 0;
+            int Wage_Per_Day = 0;
+            int Monthly_Wage = 0;
+            int Total_Working_Hrs = 0;
+            int Present_Days = 0;
 
-            while (total_working_hrs <= max_working_hrs && present_days <= working_days)
+            while (Total_Working_Hrs <= Max_Working_Hrs && Present_Days <= Working_Days)
             {
-                Random check = new Random();
-                int status = check.Next(0, 3);
+                Random Check = new Random();
+                int Status = Check.Next(0, 3);
 
-                switch (status)
+                switch (Status)
                 {
-                    case full_time:
-                        full_day_hr = 8;
-                        present_days++;
+                    case Full_Time:
+                        Full_Day_Hr = 8;
+                        Present_Days++;
                         break;
-                    case part_time:
-                        full_day_hr = 4;
-                        present_days++;
+                    case Part_Time:
+                        Full_Day_Hr = 4;
+                        Present_Days++;
                         break;
                     default:
-                        full_day_hr = 0;
+                        Full_Day_Hr = 0;
                         break;
                 }
-                total_working_hrs += full_day_hr;
-                wage_per_day = (wage_per_hr * full_day_hr);
-                monthly_wage += wage_per_day;
-                Console.WriteLine("Daily wage = " + wage_per_day);
+                Total_Working_Hrs += Full_Day_Hr;
+                Wage_Per_Day = (Wage_Per_Hr * Full_Day_Hr);
+                Monthly_Wage += Wage_Per_Day;
+                Console.WriteLine("Daily wage = " + Wage_Per_Day);
             }
-            Console.WriteLine("Total hours worked = " + total_working_hrs);
-            Console.WriteLine("Total days present = " + present_days);
-            Console.WriteLine("Monthly wage = " + monthly_wage);
+            Console.WriteLine("Total hours worked = " + Total_Working_Hrs);
+            Console.WriteLine("Total days present = " + Present_Days);
+            Console.WriteLine("Monthly wage = " + Monthly_Wage);
         }
     }
 }
