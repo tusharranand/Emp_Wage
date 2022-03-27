fc25821010a90e8852213605d1d5ae8344dc0571
Tushar Anand <tusharranand@gmail.com>
2022-03-27 13:30:46 +0530

    [Tushar Anand] Added: Array list in place of array

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 
-namespace UC8_EmpWage
+namespace EmpWage
 {
     class Company
     {
@@ -23,36 +23,37 @@ namespace UC8_EmpWage
         }
 
     }
-
-    class Wage_Computation
+    public interface ICompute_Emp_Wage
+    {
+        public void AddCompany(string Company_Name, int Wage_Per_Hr, int Full_Hr_Per_Day,
+            int Part_Hr_Per_Day, int Max_Working_Hrs, int Max_Working_Days);
+        public void Calculations(string Company_Name);
+    }
+    class Wage_Computation : ICompute_Emp_Wage
     {
         public const int Full_Time = 1;
         public const int Part_Time = 2;
         private Dictionary<string, Company> Companies_Dict;
-        private string[] Company_List;
-        public int Company_Index = 0;
+        private ArrayList Company_List;
+        private int No_of_Companies;
 
-        public Wage_Computation(int n)
+        public Wage_Computation()
         {
             Companies_Dict = new Dictionary<string, Company>();
-            Company_List = new string[2*n];
+            Company_List = new ArrayList();
         }
-
-        public void AddCompany(string Company_Name, int Wage_Per_Hr, int Full_Hr_Per_Day, 
+        public void AddCompany(string Company_Name, int Wage_Per_Hr, int Full_Hr_Per_Day,
             int Part_Hr_Per_Day, int Max_Working_Hrs, int Max_Working_Days)
         {
-            Company comp_obj = new Company(Company_Name.ToLower(), Wage_Per_Hr, Full_Hr_Per_Day, 
+            Company comp_obj = new Company(Company_Name.ToLower(), Wage_Per_Hr, Full_Hr_Per_Day,
                 Part_Hr_Per_Day, Max_Working_Hrs, Max_Working_Days);
             Companies_Dict.Add(Company_Name.ToLower(), comp_obj);
-            Company_List[Company_Index] = Company_Name;
-            Company_Index++;
+            Company_List.Add(Company_Name);
         }
-
         private int Present_Check()
         {
             return new Random().Next(0, 3);
         }
-
         public void Calculations(string Company_Name)
         {
             int Total_Working_Hrs = 0;
@@ -65,7 +66,7 @@ namespace UC8_EmpWage
                 throw new ArgumentNullException("Company doesn't Exist!");
             Companies_Dict.TryGetValue(Company_Name.ToLower(), out Company comp_obj);
 
-            while (Total_Working_Hrs < comp_obj.Max_Working_Hrs && 
+            while (Total_Working_Hrs < comp_obj.Max_Working_Hrs &&
                 Present_Days < comp_obj.Max_Working_Days)
             {
 
@@ -87,22 +88,21 @@ namespace UC8_EmpWage
                 Wage_Per_Day = (comp_obj.Wage_Per_Hr * Hr_Per_Day);
                 Monthly_Wage += Wage_Per_Day;
             }
-            Company_List[Company_Index] = Convert.ToString(Monthly_Wage);
-            Company_Index++;
+            Company_List.Add(Monthly_Wage);
         }
-        public void View_List()
+
+        public void View_Wage()
         {
-            for (int i = 0; i < Company_List.Length; i += 2)
+            for (int i = 0; i < Company_List.Count; i += 2)
             {
-                Console.WriteLine("For {0} monthly wage is {1}.", Company_List[i], Company_List[i+1]);
+                Console.WriteLine("Monthly wage for {0} is {1}", Company_List[i], Company_List[i + 1]);
             }
         }
 
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program.");
-            
-            Wage_Computation Employee = new Wage_Computation(3);
+            Wage_Computation Employee = new Wage_Computation();
 
             Employee.AddCompany("TATA", 20, 8, 4, 100, 20);
             Employee.Calculations("Tata");
@@ -110,10 +110,10 @@ namespace UC8_EmpWage
             Employee.AddCompany("Mahindra", 30, 8, 4, 100, 20);
             Employee.Calculations("Mahindra");
 
-            Employee.AddCompany("DMart", 30, 8, 4, 100, 20);
-            Employee.Calculations("DMart");
+            Employee.AddCompany("DMart", 40, 9, 5, 100, 20);
+            Employee.Calculations("Dmart");
 
-            Employee.View_List();
+            Employee.View_Wage();
         }
     }
 }
